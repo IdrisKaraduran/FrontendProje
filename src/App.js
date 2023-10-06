33c3244ffc594f203a143e69f8d34ffc9c527841
idris <ninoreyesmanuel@gmail.com>
2023-10-06 22:57:11 +0200

    ErsteTag

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import Topbar from './components/common/topbar'
+import Menubar from './components/common/menubar'
+import Footer from './components/common/footer'
+
+const App = () => {
+  return (
+    <>
+    <Topbar />
+    <Menubar/>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+    <p>.</p>
+
+
+    <Footer/>
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
